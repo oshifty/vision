f84843399d5f9bae43a67b8c4f273be60e55ce9f
Lukas Runge <login@lukasrunge.de>
2023-08-20 15:23:18 +0200

    ✨💚 added automatic fetching of udr schema

---
+++ src/libraries/helpers.ts
@@ -0,0 +1,7 @@
+export function Try<T>(fn: () => T, err: (e: Error) => any): T {
+	try {
+		return fn();
+	} catch (e) {
+		return err(e as Error);
+	}
+}
--- src/vision.ts
+++ src/vision.ts
@@ -1,3 +1,4 @@
+export * from "./libraries/udr";
 export { IProps } from "./interfaces/IProps";
 export { handleBars } from "./libraries/handleBars";
 export { MyClass } from "./modules/MyClass";
