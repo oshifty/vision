5dcb3d61dd9861721fa0f42ab555c4cb51f17eee
Lukas Runge <login@lukasrunge.de>
2023-08-27 19:18:00 +0200

    ✨ added map function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,6 +6,10 @@ export function Try<T>(fn: () => T, err: (e: Error) => T): T {
 	}
 }
 
+export function map(x: number, in_min: number, in_max: number, out_min: number, out_max: number) {
+	return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
+
 export function add(a: number, b: number): number {
 	return a + b;
 }
