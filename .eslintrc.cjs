a2c01f11048d27fb5930c7109376fff17f4dc150
Lukas Runge <login@lukasrunge.de>
2023-08-28 05:18:43 +0200

    💄 eslint: no-unused-vars now ignoring underscores

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,5 +26,17 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	rules: {
+		// note you must disable the base rule as it can report incorrect errors
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn', // or "error"
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_'
+			}
+		]
+	}
 };
