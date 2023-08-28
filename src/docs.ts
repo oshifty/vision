f1ec8da38cf0762d274f710c38bd73917685f9a1
Lukas Runge <login@lukasrunge.de>
2023-08-28 05:25:46 +0200

    ✨📚 added new types to docs

--- src/docs.ts
+++ src/docs.ts
@@ -4,9 +4,21 @@
 export * as UDR from './lib/udr';
 
 /**
- * E1.73 Extended by this Repository
+ * E1.73 extended by this Repository
  */
 export * as UDRnext from './lib/udr.next';
+
+/**
+ * GDTF (General Device Type Format)
+ */
+export * as GDTF from 'gdtf-types';
+
+/**
+ * Helper functions for working with GDTF (General Device Type Format)
+ */
+export * as GDTFHelpers from './lib/gdtfHelpers';
+
+export { World } from './modules/World';
 
 export { Fixture } from './modules/Fixture';
 
